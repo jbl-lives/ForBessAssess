459d457330efd5b7c85717424195054987de3e50
jabu <jbllives@gmail.com>
2023-12-08 11:33:50 +0200

    Installing the get machine method

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def insert_location():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+
 @app.route('/insert_inverter', methods=['POST'])
 def insert_inverter():
     try:
@@ -67,3 +68,45 @@ def insert_inverter():
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+
+@app.route('/get_machine', methods=['GET', 'POST'])
+def get_machine():
+    try:
+        if request.method == 'GET':
+            q_code = request.args.get('QrCode')
+        elif request.method == 'POST':
+            q_code = request.form.get('QrCode')
+        else:
+            return 'Method not allowed', 405
+        # q_code = "QR78902909"
+        sql = "SELECT qr_code, inverter_manufacturer, inverter_capacity, serial_number, \
+                      installation_date, battery_manufacturer, battery_type, battery_capacity, \
+                      num_of_batteries, battery_serial, tenant_name, premise_address, \
+                      contact_person, contact_number, inspection_date, inspector_name \
+               FROM inverters\
+               INNER JOIN locations\
+               ON locations.location_id = inverters.inverter_id\
+               WHERE qr_code = %s;"
+
+        mycursor.execute(sql, (q_code,))
+        result = mycursor.fetchone()
+
+        if result:
+            # Use variables directly
+            qr_code, inverter_manufacturer, inverter_capacity, serial_number, \
+                installation_date, battery_manufacturer, battery_type, battery_capacity, \
+                num_of_batteries, battery_serial,  tenant_name, premise_address, \
+                contact_person, contact_number, inspection_date, inspector_name = result
+
+            return (
+                f"{qr_code}|{inverter_manufacturer}|{inverter_capacity}|{serial_number}|"
+                f"{installation_date}|{battery_manufacturer}|{battery_type}|{battery_capacity}|"
+                f"{num_of_batteries}|{battery_serial}|{tenant_name}|"
+                f"{premise_address}|{contact_person}|{contact_number}|"
+                f"{inspection_date}|{inspector_name}"
+            ), 200
+        else:
+            return 'Game not found', 404
+    except Exception as e:
+        return str(e), 500
