afbbe13b52506ff09896e125578f3bd29c5cc48e
jabu <jbllives@gmail.com>
2023-12-08 09:33:07 +0200

    Clearing the project

---
+++ app.py
@@ -0,0 +1,15 @@
+
+#from flask import Flask, request, jsonify
+#import mysql.connector
+
+# initialize the flask application
+#app = Flask(__name__)
+
+# Create database connection
+#conn = mysql.connector.connect(
+ #   host="roundhouse.proxy.rlwy.net",
+ #   user="root",
+ #   password="aCa4-GehdH31BGHe1fDh4H4hagdC1hD5",
+ #   database="railway",
+ #   port=14241
+#)
